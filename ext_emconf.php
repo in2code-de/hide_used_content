fab78277cd2bc7bb03880874f9072015a3d2941d
Florian Langer <florian.langer@in2code.de>
2026-07-21 17:04:29 +0200

    [TASK] Allow TYPO3 v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'typo3@cordes.co',
     'author_company' => 'biz-design',
     'state' => 'stable',
-    'version' => '2.0.0',
+    'version' => '2.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-13.4.99',
+            'typo3' => '12.0.0-14.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
